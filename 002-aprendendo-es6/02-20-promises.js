107f5f942a9bdbfbb6ff99170fe8a1a7a0fb8800
Filipe Lemos <contato@filipelemos.com>
2018-04-09 20:00:41 -0300

    Promises e Fetch

--- 002-aprendendo-es6/02-20-promises.js
+++ 002-aprendendo-es6/02-20-promises.js
@@ -1,3 +1,24 @@
 /*
-
+  Promises são objetos que contém ações que serão finalizadas em um futuro. Daí o nome de promessa.
+  Facilita no trabalho com dados assíncronos.
+  Recebe dois parâmetros: um resolve() para efetuar a ação e um reject() para negá-la caso haja alguma exceção.
 */
+
+let promessa = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (true)
+      resolve('Promise funcionando')
+    else
+      reject('Promise não funcionou')
+  }, 3000);
+});
+
+// O método then() executa a promise no caso de sucesso, enquanto o catch() executa no casso de falha.
+// É permitido o uso de métodos then() aninhados.
+promessa
+  .then((data) => {
+    console.log(data);
+    return 'Filipe';
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
---
+++ 002-aprendendo-es6/02-21-promises-fetch.js
@@ -0,0 +1,13 @@
+/*
+  A Fetch API é uma alternativa que visa atribuir a praticidade de trabalho das promises ao AJAX.
+  Possui 3 estados: pending, resolved e rejected
+*/
+
+var fetch = require('node-fetch');
+
+fetch('https://api.github.com/users/filipeltsilva/repos')
+  .then((data) => data.json())
+  .then((data) => data.map((repo) => {
+    console.log(repo.full_name);
+  }))
+  .catch((error) => console.log(error));
