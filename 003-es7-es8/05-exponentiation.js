0452b6e70ba0aef53b505aa8d6816e9fc7d52fb7
Filipe Lemos <contato@filipelemos.com>
2018-05-18 14:42:37 -0300

    Exponenciação de números

---
+++ 003-es7-es8/05-exponentiation.js
@@ -0,0 +1,5 @@
+// Forma antiga de trabalhar com potenciação de números
+console.log(Math.pow(2, 10));
+
+// Nova forma de utilizar a potenciação, com dois sinais de multiplicação (**)
+console.log(2 ** 10);
