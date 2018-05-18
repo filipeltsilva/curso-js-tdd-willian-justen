41b08a2745ef6ec7b0ac36d6d58fecbbcfe06535
Filipe Lemos <contato@filipelemos.com>
2018-05-18 11:51:56 -0300

    padStart() e padEnd()

---
+++ 003-es7-es8/04-padstart-padend.js
@@ -0,0 +1,7 @@
+/*
+  padStart preenche uma string em seu início n vezes com o caracter passado no parâmetro.
+  padEnd preenche uma string em seu final n vezes com o caracter passado no parâmetro.
+*/
+
+console.log(''.padStart(8, 'Lá') + ' Silvio Santos vem aí ' + ''.padEnd(9, 'OlêOlêOlá'));
+
