8b0c5e26ee1c41aeff17c93c9b2164e17919410b
Filipe Lemos <contato@filipelemos.com>
2018-06-02 18:52:52 -0300

    Uglify no Webpack

--- 004-modulos-js/webpack.config.js
+++ 004-modulos-js/webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false }
+    })
+  ]
 }
 
